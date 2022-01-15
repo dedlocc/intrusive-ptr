943dd14867f3efabe8583a8c61235b2d57ce1475
Artem Koton <kotonartem@protonmail.com>
2022-01-15 14:16:25 +0300

    Implement intrusive-ptr

--- solution.h
+++ solution.h
@@ -1,89 +1,200 @@
+#include <atomic>
+#include <concepts>
+
+template <typename T>
+struct intrusive_ref_counter
+{
+    intrusive_ref_counter() noexcept = default;
+    intrusive_ref_counter(intrusive_ref_counter const &v) noexcept
+    {}
+
+    intrusive_ref_counter &operator=(intrusive_ref_counter const &) noexcept
+    {
+        return *this;
+    }
+
+    std::size_t use_count() const noexcept
+    {
+        return _counter;
+    }
+
+    friend void intrusive_ptr_add_ref(intrusive_ref_counter const *p) noexcept
+    {
+        p->_counter.fetch_add(1, std::memory_order_relaxed);
+    }
+
+    friend void intrusive_ptr_release(intrusive_ref_counter const *p) noexcept
+    {
+        if (p->_counter.fetch_sub(1, std::memory_order_acq_rel) == 1) {
+            delete static_cast<const T*>(p);
+        }
+    }
+
+protected:
+    ~intrusive_ref_counter() = default;
+
+private:
+    mutable std::atomic_size_t _counter = 0;
+};
+
 template <typename T>
 struct intrusive_ptr
 {
     using element_type = T;
 
-    intrusive_ptr() noexcept;
-    intrusive_ptr(T *p, bool add_ref = true);
+    intrusive_ptr() noexcept = default;
 
-    intrusive_ptr(intrusive_ptr const &r);
+    intrusive_ptr(element_type *ptr, bool add_ref = true) : ptr(ptr)
+    {
+        if (ptr && add_ref) {
+            intrusive_ptr_add_ref(ptr);
+        }
+    }
 
-    template <class Y>
-    intrusive_ptr(intrusive_ptr<Y> const &r);
-
-    intrusive_ptr(intrusive_ptr &&r);
-
-    template <class Y>
-    intrusive_ptr(intrusive_ptr<Y> &&r);
-
-    ~intrusive_ptr();
-
-    intrusive_ptr &operator=(intrusive_ptr const &r);
+    intrusive_ptr(intrusive_ptr const &r) : intrusive_ptr(r.get())
+    {}
 
     template <class Y>
-    intrusive_ptr &operator=(intrusive_ptr<Y> const &r);
+    requires std::convertible_to<Y *, T *>
+    intrusive_ptr(intrusive_ptr<Y> const &r) : intrusive_ptr(r.get())
+    {}
 
-    intrusive_ptr &operator=(T *r);
-
-    intrusive_ptr &operator=(intrusive_ptr &&r);
+    intrusive_ptr(intrusive_ptr &&r) : ptr(std::exchange(r.ptr, nullptr))
+    {}
 
     template <class Y>
-    intrusive_ptr &operator=(intrusive_ptr<Y> &&r);
+    intrusive_ptr(intrusive_ptr<Y> &&r) : ptr(std::exchange(r.ptr, nullptr))
+    {}
 
-    void reset();
-    void reset(T *r);
-    void reset(T *r, bool add_ref);
+    ~intrusive_ptr()
+    {
+        if (ptr) {
+            intrusive_ptr_release(ptr);
+        }
+    }
 
-    T &operator*() const noexcept;
-    T *operator->() const noexcept;
-    T *get() const noexcept;
-    T *detach() noexcept;
+    intrusive_ptr &operator=(intrusive_ptr const &r)
+    {
+        return operator=<T>(r);
+    }
 
-    explicit operator bool() const noexcept;
+    template <class Y>
+    requires std::convertible_to<Y *, T *>
+    intrusive_ptr &operator=(intrusive_ptr<Y> const &r)
+    {
+        return operator=(r.get());
+    }
 
-    void swap(intrusive_ptr &b) noexcept;
+    intrusive_ptr &operator=(element_type *r)
+    {
+        intrusive_ptr(r).swap(*this);
+        return *this;
+    }
+
+    intrusive_ptr &operator=(intrusive_ptr &&r)
+    {
+        return operator=<T>(std::move(r));
+    }
+
+    template <class Y>
+    requires std::convertible_to<Y *, T *>
+    intrusive_ptr &operator=(intrusive_ptr<Y> &&r)
+    {
+        intrusive_ptr(std::move(r)).swap(*this);
+        return *this;
+    }
+
+    void reset()
+    {
+        intrusive_ptr().swap(*this);
+    }
+
+    void reset(element_type *r, bool add_ref = true)
+    {
+        intrusive_ptr(r, add_ref).swap(*this);
+    }
+
+    element_type &operator*() const noexcept
+    {
+        return *get();
+    }
+
+    element_type *operator->() const noexcept
+    {
+        return get();
+    }
+
+    element_type *get() const noexcept
+    {
+        return ptr;
+    }
+
+    element_type *detach() noexcept
+    {
+        return std::exchange(ptr, nullptr);
+    }
+
+    explicit operator bool() const noexcept
+    {
+        return get() != 0;
+    }
+
+    void swap(intrusive_ptr &b) noexcept
+    {
+        std::swap(ptr, b.ptr);
+    }
+
+private:
+    element_type *ptr = nullptr;
+
+    template <typename Y>
+    friend struct intrusive_ptr;
 };
 
 template <class T, class U>
-bool operator==(intrusive_ptr<T> const &a, intrusive_ptr<U> const &b) noexcept;
-
-template <class T, class U>
-bool operator!=(intrusive_ptr<T> const &a, intrusive_ptr<U> const &b) noexcept;
-
-template <class T, class U>
-bool operator==(intrusive_ptr<T> const &a, U *b) noexcept;
-
-template <class T, class U>
-bool operator!=(intrusive_ptr<T> const &a, U *b) noexcept;
-
-template <class T, class U>
-bool operator==(T *a, intrusive_ptr<U> const &b) noexcept;
-
-template <class T, class U>
-bool operator!=(T *a, intrusive_ptr<U> const &b) noexcept;
-
-template <class T>
-bool operator<(intrusive_ptr<T> const &a, intrusive_ptr<T> const &b) noexcept;
-
-template <class T>
-void swap(intrusive_ptr<T> &a, intrusive_ptr<T> &b) noexcept;
-
-template <typename T>
-struct intrusive_ref_counter
+bool operator==(intrusive_ptr<T> const &a, intrusive_ptr<U> const &b) noexcept
 {
-    intrusive_ref_counter() noexcept;
-    intrusive_ref_counter(const intrusive_ref_counter &v) noexcept;
+    return a.get() == b.get();
+}
 
-    intrusive_ref_counter &operator=(const intrusive_ref_counter &v) noexcept;
+template <class T, class U>
+bool operator!=(intrusive_ptr<T> const &a, intrusive_ptr<U> const &b) noexcept
+{
+    return a.get() != b.get();
+}
 
-    unsigned int use_count() const noexcept;
+template <class T, class U>
+bool operator==(intrusive_ptr<T> const &a, U *b) noexcept
+{
+    return a.get() == b;
+}
 
-protected:
-    ~intrusive_ref_counter() = default;
-};
+template <class T, class U>
+bool operator!=(intrusive_ptr<T> const &a, U *b) noexcept
+{
+    return a.get() != b;
+}
 
-template <class Derived>
-void intrusive_ptr_add_ref(const intrusive_ref_counter<Derived> *p) noexcept;
+template <class T, class U>
+bool operator==(T *a, intrusive_ptr<U> const &b) noexcept
+{
+    return a == b.get();
+}
 
-template <class Derived>
-void intrusive_ptr_release(const intrusive_ref_counter<Derived> *p) noexcept;
+template <class T, class U>
+bool operator!=(T *a, intrusive_ptr<U> const &b) noexcept
+{
+    return a != b.get();
+}
+
+template <class T>
+bool operator<(intrusive_ptr<T> const &a, intrusive_ptr<T> const &b) noexcept
+{
+    return std::less(a.get(), b.get());
+}
+
+template <class T>
+void swap(intrusive_ptr<T> &a, intrusive_ptr<T> &b) noexcept
+{
+    a.swap(b);
+}
